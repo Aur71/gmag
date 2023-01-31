5d643f0916c11686e11c58bac5159bcbe0fcd5ab
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-31 14:38:33 +0200

    global context added

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import '@/styles/globals.scss';
+import { AppProvider } from '@/context/globalContext';
 import Navbar from '@/components/layout/navbar/Navbar';
 import Sidebar from '@/components/layout/sidebar/Sidebar';
 import Footer from '@/components/layout/footer/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <AppProvider>
       <Navbar />
       <Sidebar />
       <main>
         <Component {...pageProps} />
       </main>
       <Footer />
-    </>
+    </AppProvider>
   );
 }
