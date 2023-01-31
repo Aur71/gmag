c01f24646db06518cd48bdd316ab42c54dcf78e1
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-31 16:12:51 +0200

    Navbar design done

--- components/layout/navbar/Navbar.jsx
+++ components/layout/navbar/Navbar.jsx
@@ -1,7 +1,55 @@
 import styles from './Navbar.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../../public/logo.png';
+import { AiOutlineMenuFold } from 'react-icons/ai';
+import { CiSearch } from 'react-icons/ci';
+import { BsSuitHeart, BsCart3 } from 'react-icons/bs';
+import userImg from '../../../public/user.png';
 
 const Navbar = () => {
-  return <nav className={styles.navbar}>Navbar</nav>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <button>
+          <AiOutlineMenuFold />
+        </button>
+
+        <Link href='/'>
+          <Image src={logo} alt='logo' />
+        </Link>
+      </div>
+
+      <div className={styles.search_container}>
+        <input type='text' placeholder='Search...' />
+
+        <button>
+          <CiSearch />
+        </button>
+      </div>
+
+      <ul className={styles.icons_container}>
+        <li className={styles.favorites_link}>
+          <Link href='/favorites'>
+            <BsSuitHeart />
+          </Link>
+        </li>
+
+        <li className={styles.cart_link}>
+          <Link href='/cart'>
+            <BsCart3 />
+            <span>5</span>
+          </Link>
+        </li>
+
+        <li className={styles.account_link}>
+          <Link href='/account/id'>
+            <Image src={userImg} alt='default user img' />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
