7cfddb88548b92b0bc0c5607b9652f88814d8532
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-02-07 11:33:50 +0200

    card sliders added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import PromotionSlider from '@/components/sliders/promotion_slider/PromotionSlider';
+import ProductsSlider from '@/components/sliders/products_slider/ProductsSlider';
+
+import { ImFire } from 'react-icons/im';
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
       <PromotionSlider />
+      <ProductsSlider title='Hot Deals' icon={<ImFire className='red' />} />
     </div>
   );
 }
