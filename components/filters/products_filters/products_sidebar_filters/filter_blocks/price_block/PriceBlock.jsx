2c0fa6fd887b2893c6a32fd5ac673fadb7d4ee4e
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-02-21 18:31:21 +0200

    price range added

--- components/filters/products_filters/products_sidebar_filters/filter_blocks/price_block/PriceBlock.jsx
+++ components/filters/products_filters/products_sidebar_filters/filter_blocks/price_block/PriceBlock.jsx
@@ -1,14 +1,68 @@
 import styles from './PriceBlock.module.scss';
+import { useState } from 'react';
 import BlockHeader from '../block_components/block_header/BlockHeader';
+import ReactSlider from 'react-slider';
 
 const PriceBlock = ({ name, options }) => {
-  console.log('PRICE BLOCK', options);
+  const [min, setMin] = useState(options.min);
+  const [max, setMax] = useState(options.max);
+
+  function handleSliderChange(values) {
+    setMin(parseInt(values[0]));
+    setMax(parseInt(values[1]));
+  }
+
+  function handleMinChange(event) {
+    setMin(parseInt(event.target.value));
+  }
+
+  function handleMaxChange(event) {
+    setMax(parseInt(event.target.value));
+  }
 
   return (
     <div className={styles.price_block}>
       <BlockHeader name={name} />
 
-      <div className={styles.options}></div>
+      <div className={styles.options}>
+        <ReactSlider
+          defaultValue={[min, max]}
+          className={styles.slider}
+          trackClassName={styles.tracker}
+          min={options.min}
+          max={options.max}
+          minDistance={50}
+          step={50}
+          pearling={true}
+          renderThumb={(props) => {
+            return <div {...props} className={styles.thumb}></div>;
+          }}
+          renderTrack={(props) => {
+            return <div {...props} className={styles.track}></div>;
+          }}
+          onChange={handleSliderChange}
+        />
+
+        <div className={styles.inputs_container}>
+          <input
+            type='number'
+            min={options.min}
+            max={options.max}
+            value={min}
+            onChange={handleMinChange}
+          />
+
+          <span>-</span>
+
+          <input
+            type='number'
+            min={options.min}
+            max={options.max}
+            value={max}
+            onChange={handleMaxChange}
+          />
+        </div>
+      </div>
     </div>
   );
 };
