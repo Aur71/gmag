506d141cccadef7c92045d20e008caec5f6e0199
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-02-25 15:13:01 +0200

    pagination added

--- components/filters/products_filters/products_pagination/ProductsPagination.jsx/ProductsPagination.jsx
+++ components/filters/products_filters/products_pagination/ProductsPagination.jsx/ProductsPagination.jsx
@@ -1,7 +1,87 @@
 import styles from './ProductsPagination.module.scss';
+import { useState, useRef, useEffect } from 'react';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
 const ProductsPagination = () => {
-  return <div className={styles.products_pagination}>ProductsPagination</div>;
+  const buttonsContainerRef = useRef(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pages = 20;
+  const buttons = [];
+
+  const handlePage = (num) => {
+    setCurrentPage(num);
+  };
+
+  const checkNum = (num) => {
+    if (num <= 0) return 1;
+    if (num > pages) return pages;
+    return num;
+  };
+
+  const pageDecrease = () => {
+    setCurrentPage(checkNum(currentPage - 1));
+    const buttonWidth =
+      buttonsContainerRef.current.children[0].getBoundingClientRect().width;
+    buttonsContainerRef.current.scrollLeft -= buttonWidth;
+  };
+
+  const pageIncrease = () => {
+    setCurrentPage(checkNum(currentPage + 1));
+    const buttonWidth =
+      buttonsContainerRef.current.children[0].getBoundingClientRect().width;
+    buttonsContainerRef.current.scrollLeft += buttonWidth;
+  };
+
+  // RENDERING BUTTONS BASED ON THE NUMBER OF PAGES
+  // PROBLEM WITH THE WIDTH WHEN THERE ARE MORE THAN 1 CHARACTER
+  for (let i = 1; i <= pages; i++) {
+    buttons.push(
+      <button key={i} onClick={() => handlePage(i)}>
+        <span className={`${currentPage === i && styles.active}`}>{i}</span>
+      </button>
+  }
+
+  useEffect(() => {
+    // SETTING THE WIDTH OF THE CONTAINER ON THE INITIAL RENDER
+    const buttonWidth =
+      buttonsContainerRef.current.children[0].getBoundingClientRect().width;
+
+    if (pages <= 3) {
+      buttonsContainerRef.current.style.width = `${pages * buttonWidth}px`;
+    } else {
+      buttonsContainerRef.current.style.width = `${3 * buttonWidth}px`;
+    }
+  }, []);
+
+  return (
+    <div className={styles.products_pagination}>
+      <button onClick={pageDecrease}>
+        <AiOutlineLeft className={styles.icon} />
+      </button>
+
+      {currentPage >= pages - 3 ? (
+        <>
+          <button onClick={() => handlePage(1)}>1</button>
+          <button className={styles.disabled}>...</button>
+        </>
+      ) : null}
+
+      <div className={styles.buttons_container} ref={buttonsContainerRef}>
+        {buttons}
+      </div>
+
+      {currentPage <= pages - 3 ? (
+        <>
+          <button className={styles.disabled}>...</button>
+          <button onClick={() => handlePage(20)}>{pages}</button>
+        </>
+      ) : null}
+
+      <button onClick={pageIncrease}>
+        <AiOutlineRight className={styles.icon} />
+      </button>
+    </div>
+  );
 };
 
 export default ProductsPagination;
