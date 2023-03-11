81b694e1f185f6bfa84161e16c09dfebbd56c2cd
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-03-11 16:19:13 +0200

    x

--- components/product_details_components/product_showcase/slider/Slider.jsx
+++ components/product_details_components/product_showcase/slider/Slider.jsx
@@ -13,38 +13,86 @@ import {
 import img1 from '../../../../public/temp/computer.png';
 import img2 from '../../../../public/temp/laptop.png';
 
+const images = [
+  {
+    name: 'img1',
+    img: img1,
+  },
+  {
+    name: 'img2',
+    img: img2,
+  },
+  {
+    name: 'img3',
+    img: img1,
+  },
+  {
+    name: 'img4',
+    img: img2,
+  },
+  {
+    name: 'img5',
+    img: img1,
+  },
+  {
+    name: 'img6',
+    img: img2,
+  },
+  {
+    name: 'img7',
+    img: img2,
+  },
+  {
+    name: 'img8',
+    img: img1,
+  },
+  {
+    name: 'img9',
+    img: img2,
+  },
+  {
+    name: 'img10',
+    img: img1,
+  },
+  {
+    name: 'img11',
+    img: img2,
+  },
+];
+
 const Slider = () => {
   const [windowWidth, setWindowWidth] = useState(0);
   const btnsSliderRef = useRef(null);
   const { events } = useDraggable(btnsSliderRef);
   const [disableTopLeftBtn, setDisableTopLeftBtn] = useState(false);
   const [disableBottomRightBtn, setDisableBottomRightBtn] = useState(false);
+  const [activeImage, setActiveImage] = useState(0);
 
   const slideUp = () => {
     const sliderHeight = btnsSliderRef.current.getBoundingClientRect().height;
     btnsSliderRef.current.style.scrollBehavior = 'smooth';
-    btnsSliderRef.current.scrollTop -= sliderHeight;
+    btnsSliderRef.current.scrollTop -= sliderHeight - 50;
     btnsSliderRef.current.style.scrollBehavior = 'auto';
   };
 
   const slideDown = () => {
     const sliderHeight = btnsSliderRef.current.getBoundingClientRect().height;
     btnsSliderRef.current.style.scrollBehavior = 'smooth';
-    btnsSliderRef.current.scrollTop += sliderHeight;
+    btnsSliderRef.current.scrollTop += sliderHeight - 50;
     btnsSliderRef.current.style.scrollBehavior = 'auto';
   };
 
   const slideLeft = () => {
     const sliderWidth = btnsSliderRef.current.getBoundingClientRect().width;
     btnsSliderRef.current.style.scrollBehavior = 'smooth';
-    btnsSliderRef.current.scrollLeft -= sliderWidth;
+    btnsSliderRef.current.scrollLeft -= sliderWidth - 50;
     btnsSliderRef.current.style.scrollBehavior = 'auto';
   };
 
   const slideRight = () => {
     const sliderWidth = btnsSliderRef.current.getBoundingClientRect().width;
     btnsSliderRef.current.style.scrollBehavior = 'smooth';
-    btnsSliderRef.current.scrollLeft += sliderWidth;
+    btnsSliderRef.current.scrollLeft += sliderWidth - 50;
     btnsSliderRef.current.style.scrollBehavior = 'auto';
   };
 
@@ -121,78 +169,25 @@ const Slider = () => {
           {...events}
           onScroll={handleScroll}
         >
-          <button className={`${styles.img_btn} ${styles.active}`}>
-            <Image
-              src={img1}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
-          <button className={styles.img_btn}>
-            <Image
-              src={img2}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
-          <button className={styles.img_btn}>
-            <Image
-              src={img1}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
-          <button className={styles.img_btn}>
-            <Image
-              src={img2}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
-          <button className={styles.img_btn}>
-            <Image
-              src={img1}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
-          <button className={styles.img_btn}>
-            <Image
-              src={img2}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
-          <button className={styles.img_btn}>
-            <Image
-              src={img1}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
-          <button className={styles.img_btn}>
-            <Image
-              src={img2}
-              alt='temp'
-              width={100}
-              height={100}
-              priority={true}
-            />
-          </button>
+          {images.map((image, index) => {
+            return (
+              <button
+                key={index}
+                className={`${styles.img_btn} ${
+                  activeImage === index && styles.active
+                }`}
+                onMouseOver={() => setActiveImage(index)}
+              >
+                <Image
+                  src={image.img}
+                  alt={image.name}
+                  width={100}
+                  height={100}
+                  priority={true}
+                />
+              </button>
+            );
+          })}
         </div>
 
         <button
@@ -207,7 +202,7 @@ const Slider = () => {
 
       <div className={styles.img_container}>
         <Image
-          src={img1}
+          src={images[activeImage].img}
           alt='temporary img'
           width={500}
           height={500}
