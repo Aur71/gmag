2e48db61feea197ac98b1f47261e4a30ae70e8fa
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-03-14 16:48:24 +0200

    Image viewer sidebar design done

--- components/product_details_components/image_viewer/sidebar/Sidebar.jsx
+++ components/product_details_components/image_viewer/sidebar/Sidebar.jsx
@@ -1,7 +1,50 @@
+import Image from 'next/image';
 import styles from './Sidebar.module.scss';
 
+// TEMP IMG
+import img1 from '../../../../public/temp/computer.png';
+import img2 from '../../../../public/temp/laptop.png';
+
 const Sidebar = () => {
-  return <aside className={styles.sidebar}>Sidebar</aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.btns_container}>
+        <button className={styles.active}>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img2} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img2} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img2} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+        <button>
+          <Image src={img1} alt='temp image' width={100} height={100} />
+        </button>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
