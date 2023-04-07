452d4184f199086afe5310ac3b6983ceaa8a4470
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-04-07 17:14:35 +0300

    ticket done

--- components/cart_components/ticket/Ticket.jsx
+++ components/cart_components/ticket/Ticket.jsx
@@ -1,7 +1,33 @@
 import styles from './Ticket.module.scss';
+import { HiOutlineChevronDoubleRight } from 'react-icons/hi';
 
 const Ticket = () => {
-  return <section className={styles.ticket}>Ticket</section>;
+  return (
+    <section className={styles.ticket}>
+      <h2>Order summary</h2>
+
+      <p>
+        <span>Products cost:</span>
+        <span>1999.99$</span>
+      </p>
+
+      <p>
+        <span>Delivery cost:</span>
+        <span>25$</span>
+      </p>
+
+      <h3>Total:</h3>
+      <h4>2024.99$</h4>
+
+      <button>
+        <span>
+          <HiOutlineChevronDoubleRight />
+        </span>
+
+        <span>continue</span>
+      </button>
+    </section>
+  );
 };
 
 export default Ticket;
