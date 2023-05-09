c472dd24f79cf176e65cf0ba635a57bdd536760a
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-09 17:18:07 +0300

    x

--- features/product_filtering_sidebar/ProductFilteringSidebar.jsx
+++ features/product_filtering_sidebar/ProductFilteringSidebar.jsx
@@ -1,8 +1,8 @@
 import styles from './ProductFilteringSidebar.module.scss';
 
-// finish the sidebar and add it to the search products
+const ProductFilteringSidebar = ({ products }) => {
+  console.log(products);
 
-const ProductFilteringSidebar = () => {
   return (
     <div className={styles.product_filtering_sidebar}>
       ProductFilteringSidebar
