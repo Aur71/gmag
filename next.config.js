3ca4b5f3f18e9917cecbfec1721dab423f3ec9cd
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-16 23:12:37 +0300

    Fixing font x5

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `font-src * 'unsafe-inline';`,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
