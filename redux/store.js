80d4aa96ea5547d3fc320e46076752b2a588dde0
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-20 11:58:54 +0300

    Removed useless code

--- redux/store.js
+++ redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import notificationsReducer from './reducers/notificationsSlice';
 import userReducer from './reducers/userSlice';
 import layoutReducer from './reducers/layoutSlice';
-import filtersSidebarReducer from './reducers/filtersSidebarSlice';
 import singleProductReducer from './reducers/singleProductSlice';
 import favoritesReducer from './reducers/favoritesSlice';
 import cartReducer from './reducers/cartSlice';
@@ -14,7 +13,6 @@ const store = configureStore({
     notifications: notificationsReducer,
     user: userReducer,
     layout: layoutReducer,
-    filtersSidebar: filtersSidebarReducer,
     singleProduct: singleProductReducer,
     favorites: favoritesReducer,
     cart: cartReducer,
